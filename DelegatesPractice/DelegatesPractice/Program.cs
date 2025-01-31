ea2fd71884db17c8f077c41f3a0ce7ca098cd083
sisv1der <Yarigo2018@yandex.ru>
2025-02-01 00:12:30 +0300

    практика по делегатам

---
+++ DelegatesPractice/DelegatesPractice/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace DelegatesPractice
+{
+    internal class Program
+    {
+        delegate void MathOperation(double a, double b);
+
+        static void Sum(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Substract(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Divide(double a, double b)
+        {
+            Console.WriteLine(a / b);
+        }
+        static void Main(string[] args)
+        {
+            // Простые делегаты
+            // Создай делегат, который принимает два числа и возвращает их сумму.
+            // Напиши методы для сложения, вычитания, умножения и деления,
+            // а затем используй делегат для вызова этих методов.
+
+            double a = 5.4;
+            double b = 4.3;
+
+            MathOperation math = Sum;
+            math(a, b); // 9,7
+
+            math = Substract;
+            math(a,b); // 1,1000000000000005
+
+            math = Multiply;
+            math(a,b); // 23,22
+
+            math = Divide;
+            math(a,b); // 1,2558139534883723
+
+            Console.WriteLine();
+
+            math += Multiply;
+            math += Substract;
+            math += Sum;
+
+            math(a,b); // same
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegatesPractice/MethodParameter/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace MethodParameter
+{
+    internal class Program
+    {
+        delegate double MathOperation(double a, double b);
+
+        static void DelegateReceiver(MathOperation method, double a, double b)
+        {
+            Console.WriteLine(method(a,b));
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            // Передача методов в качестве параметров
+            // Создай метод, который принимает делегат и два числа,
+            // а затем вызывает переданный метод.
+            // Используй этот метод для выполнения различных математических операций.
+
+            double a = 7.6;
+            double b = 9.2;
+
+            MathOperation math = Sum;
+            DelegateReceiver(math, a, b); // 16,799999999999997
+
+            math = Substract;
+            DelegateReceiver(math, a, b); // -1,5999999999999996
+
+            math = Multiply;
+            DelegateReceiver(math, a, b); // 69,91999999999999
+
+            math = Divide;
+            DelegateReceiver(math, a, b); // 0,8260869565217391 
+
+            Console.ReadLine();
+        }
+    }
+}
